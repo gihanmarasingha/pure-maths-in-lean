4552e780cbadc552c56effb15937bc3130802308
Gihan Marasingha <gihan.marasingha@gmail.com>
2021-07-03 14:20:27 +0100

    cleaned up additive identity level

--- src/equations/additive_identity/level.lean
+++ src/equations/additive_identity/level.lean
@@ -6,7 +6,8 @@ import myint.basic equations.backward_rewrite.level -- hide
 ## Level 9: Additive identity
 
 The Lean theorem `add_zero` states that `x + 0 = x` for every integer `x`. In mathematics, this
-property is called (right) additive identity.
+property is called (right) additive identity. Note `add_zero` should appear as a 'Theorem statement'
+in the left-hand pane. 
 -/
 
 namespace exlean -- hide
@@ -23,11 +24,9 @@ a + 0 = a
 -/
 theorem add_zero (a : ℤ) : a + 0 = a := myint.add_zero' a -- hide
 
-/- Hint: Hint for structured proof
-
-Introduce and prove the hypotheses `h2 : y + z * x = (z + x) + y` and
-`h3 : (z + x) + y = y + (z + x)` using the `have` tactic. Finish by rewriting with these
-hypotheses.
+/- 
+Your goal is to prove `zero_add`, the right additive identity property, using `add_zero`.
+Once you've done this, `zero_add` will be available to you in future levels.
 -/
 
 /- Theorem : zero_add (a : ℤ)
