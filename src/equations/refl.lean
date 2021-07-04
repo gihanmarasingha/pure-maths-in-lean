943337b70552916b8d41546f0342a58da001f851
Gihan Marasingha <gihan.marasingha@gmail.com>
2021-07-04 21:41:58 +0100

    added colours for error and warning in the text.

--- src/equations/refl.lean
+++ src/equations/refl.lean
@@ -12,10 +12,10 @@ The `refl` tactic (short for `reflexivity`) can be used to prove any statement o
 `?X = ?X`. Here, I use `?X` to stand in for any expression of any type.
 It could be `8 + 9` or `a * b` or `"adele"` or whatever.
 
-Here, you are asked to prove `x + y = x + y`, where `x` and `y` are integers.
+Below, you are asked to prove `x + y = x + y`, where `x` and `y` are integers.
 The word `sorry` between the `begin` and `end` lines below asks Lean not to give an error message if a
-proof isn't complete. You'll see a warning message in the bottom-right hand pane. This indicates
-you shouldn't trust the proof just yet, as it uses `sorry`!
+proof isn't complete. You'll see a <span style="color:orange">warning</span> message in the
+bottom-right hand pane. This indicates you shouldn't trust the proof just yet, as it uses `sorry`!
 
 Delete `sorry` (using the backspace key on your keyboard). In the right-hand pane you'll see:
 ```
@@ -29,8 +29,9 @@ are integers.
 The *target* is `⊢ x + y = x + y`. The `⊢` symbol can be read 'to prove'. So your target is
 to prove `x + y = x + y`.
 
-The bottom part of the right-hand pane shows an *error message*. Don't panic! It's just telling you
-that you haven't yet proved the result.
+The bottom part of the right-hand pane shows an <span style="color:red">error</span>  message:
+tactic failed, there are unsolved goals. Don't panic! It's just telling you that you haven't yet
+proved the result.
 
 Your task is to replace `sorry` with `refl,`. Note the comma at the end of the line!
 If you're successful, Lean will respond with the message `Proof complete!` or `no goals`.
