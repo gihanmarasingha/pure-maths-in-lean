876caaedea86f7ef21c5ab755c00280e1e157faa
Gihan Marasingha <gihan.marasingha@gmail.com>
2021-07-04 17:06:43 +0100

    rewriting at a hypothesis

--- src/equations/unique_additive_identity.lean
+++ src/equations/unique_additive_identity.lean
@@ -19,6 +19,22 @@ open_locale mynum -- hide
 
 open myint -- hide
 
+/-
+### Rewriting at a hypothesis
+
+By default, `rw add_assoc` (say) applies `add_assoc` to the target. If `h` is a hypothesis in the
+local context, `rw add_assoc at h` will rewrite `h` using `add_assoc`.
+
+In the example below, we rewrite at `h` and finish with `exact`. Alternatively, we could have
+performed a backward `rw` at the target using `rw ←add_assoc`.
+-/
+
+example (x y z : ℤ) (h : (x + y) + z = 20) : x + (y + z) = 20 :=
+begin
+  rw add_assoc at h,
+  exact h,
+end
+
 /- Hint : Hint
 Use the `specialize` tactic.
 -/
@@ -29,8 +45,8 @@ Let `e` be an integer satsifying the property `∀ (x : ℤ), x + e = x`. Then `
 theorem right_additive_identity_unique (e : ℤ) (h : ∀ (x : ℤ), x + e = x) : e = 0 :=
 begin [pure_maths]
   specialize h 0,
-  rw [←h, zero_add],
-  refl,
+  rw zero_add at h,
+  exact h,
 end
 
 end pre_group -- hide
