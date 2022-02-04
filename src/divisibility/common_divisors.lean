bfd28200901b53c147348f593df42579d7d6e64f
Gihan Marasingha <gihan.marasingha@gmail.com>
2022-02-04 18:21:22 +0000

    new material on common divisors

---
+++ src/divisibility/common_divisors.lean
@@ -0,0 +1,56 @@
+import tactic.linarith divisibility.divisibility_mul_dvd_mul  -- hide
+
+/-
+# Divisibility and Congruences
+
+## Level 3: Common divisor basics
+-/
+
+namespace exlean -- hide
+
+/-
+Let `m, n, and d` be integers. For `d` to be a _common divisor_ of `m` and `n` means that
+`d ∣ m` and `d ∣ n`.
+-/
+
+def common_divisor (d m n : ℤ) := (d ∣ m) ∧ (d ∣ n)
+
+/-
+Using the above definition, we'll show that `5` is a common divisor of `20` and `30`.
+-/
+example : common_divisor 5 20 30 :=
+begin
+  split, -- We'll show 1) `5 ∣ 20` and 2) `5 ∣ 30`.
+  { use 4,  -- 1) `⊢ 5 ∣ 20`, it suffices to show `20 = 5 * 4`.
+    norm_num, }, -- This holds by arithmetic.
+  { use 6,  -- 2) `⊢ 30 = 5 * 6`, it suffices to show `⊢ 30 = 5 * 6`.
+    norm_num, }, -- This holds by arithmetic.
+end
+
+/-
+### Tasks
+
+* Adapt the Lean proof above to show that `4` is a common divisor of `48` and `60`.
+
+* Give a handwritten proof of the same result.
+
+-/
+
+
+/- Theorem : no-side-bar
+`4` is a common divisor of `48` and `60`.
+-/
+theorem four_common_divisor_48_60 : common_divisor 4 48 60 :=
+begin
+  split,
+  { use 12,
+    norm_num, },
+  { use 15,
+    norm_num, },
+
+
+  
+end
+
+
+end exlean -- hide
---
+++ src/divisibility/common_divisors2.lean
@@ -0,0 +1,46 @@
+import tactic.linarith divisibility.common_divisors  -- hide
+
+/-
+# Divisibility and Congruences
+
+## Level 4: Common divisor results
+-/
+
+namespace exlean -- hide
+
+/-
+### Tasks
+
+In this level, you'll *decompose* the hypothesis that `d` is a common divisor of `a` and `b` to
+prove that `c` is a common divisor of `a` and `b`, under the hypothesis that `c ∣ d`.
+-/
+
+variables {d a b c : ℤ} -- hide
+
+/- Theorem : no-side-bar
+Given `d` is a common divisor of `a` and `b` and given `c ∣ d`, we have `c` is a common divisor of
+`a` and `b`.
+-/
+theorem common_divisor_of_common_divisor_of_dvd (h₁ : common_divisor d a b) (h₂ : c ∣ d) : common_divisor c a b :=
+begin
+  cases h₂ with m₃ h₃,
+  rw h₃ at h₁,
+  rcases h₁ with ⟨⟨m₄, h₄⟩, ⟨m₅, h₅⟩⟩,
+  split,
+  { use m₃ * m₄,
+    rw [h₄, mul_assoc], },
+  { use m₃ * m₅,
+    rw [h₅, mul_assoc], },
+
+  
+
+
+
+
+
+
+  
+end
+
+
+end exlean -- hide
