1baa4effa1e844c081c259bdb4937fee8246daa3
Gihan Marasingha <gihan.marasingha@gmail.com>
2022-02-12 17:57:59 +0000

    and summary and introduction

---
+++ src/logic_and_proof/and_introduction.lean
@@ -0,0 +1,53 @@
+import data.int.basic -- hide
+
+/-
+# Logic and Proof
+
+## Level 8: And introduction
+
+### The introduction principle
+
+The `split` tactic is useful when each of the constituents of a compound target are complicated.
+Via split, we create two new goals and work on each goal separately.
+
+An alternative approach to proving a statement of the form `p ∧ q` is to use the 'and introduction'
+principle. This principle combines a proof of `p` with a proof of `q` to give a proof of `p ∧ q`.
+
+**Theorem**: Let $x$ and $y$ be integers. Suppose $h_1 : x > 0$ and $h_2 : x + y = 5$. Then
+$(x > 0) \land (x + y = 5)$.
+
+**Proof**: The result follows by and introduction on $h_1$ and $h_2$.
+-/
+
+/-
+In Lean, if `h₁ : p` and `h₂ : q` then `and.intro h₁ h₂` is a proof of `p ∧ q`.
+-/
+
+example (x y : ℤ) (h₁ : x > 0) (h₂ : x + y = 5) : (x > 0) ∧ (x + y = 5) :=
+begin
+  from and.intro h₁ h₂,
+end
+
+namespace exlean -- hide
+
+/-
+### Task
+
+1. Replace `sorry` below with a one-line Lean proof, adapting the proof of the example above. Your proof
+should use `and.intro` and the `from` tactic.
+2. On a piece of paper, state and give a handwritten proof of this result.
+-/
+
+variables (p q r : Prop)
+
+/- Theorem : no-side-bar
+Let $p$, $q$, $r$ be propostions. Suppose $h_1 : p$, $h_2 : q$, and $h_3 : r$. Then $r \land q$ 
+follows.
+-/
+theorem and_intro1 (p r q : Prop) (h₁ : p) (h₂ : q) (h₃ : r) : r ∧ q :=
+begin
+  from and.intro h₃ h₂,
+
+end
+
+end exlean -- hide
---
+++ src/logic_and_proof/and_summary.lean
@@ -0,0 +1,77 @@
+import data.int.basic -- hide
+
+/-
+# Logic and Proof
+
+## Level 9: And summary
+
+### Elimination and introduction
+
+In logic, the entire meaning of $\land$ is encapsulated by two principles.
+
+1. And introduction. Given $h_1 : p$ and $h_2 : q$, and introduction on $h_1$ and $h_2$ gives a 
+proof of $p \land q$.
+2. And elimination. Given $h : p \land q$,
+    * Left and elimination on `h` gives a proof of `p`.
+    * Right and elimination on `h` gives a proof of `q`.
+
+
+In Lean, the principles are as follows.
+
+1. And introduction. Given `h₁ : p` and `h₂ : q`, `and.intro h₁ h₂` is a proof of `p ∧ q`.
+2. And elimination. Given `h : p ∧ q`,
+    * `h.left` or `and.elim_left h` gives a proof of `p`.
+    * `h.right` or `and.elim_right h` gives a proof of `q`.
+
+### `cases` and `split`, backward proof
+
+Lean provides the mechanism `cases` for doing left and right and elimination simultaneously.
+If `h : p ∧ q`, then `cases h with h₁ h₂` replaces `h` with two new hypotheses, `h₁ : p` and
+`h₂ : q`.
+
+The `split` tactic is used for 'backward' and introduction. If the target is `p ∧ q`, a forward
+application of and introduction requires having proved first `h₁ : p` and `h₂ : q`. 
+Via `split`, Lean creates two new goals, one for each of the conditions of and introduction.
+-/
+
+/-
+### `from`, `have`, and `show`
+
+* `from` is used to give a hypothesis or other proof term that closes the goal.
+* `have` allows you to create an auxiliary goal. The result of the goal is added to the context.
+* `show` is used to indicate what you're trying to prove.
+-/
+
+namespace exlean -- hide
+
+/-
+### Task
+
+1. Prove the Lean result below. Try as many different proof methods as you can. Which method
+do you like the best?
+2. On a piece of paper, state and give a handwritten proof of this result.
+3. (Bonus) Give a one-line Lean proof of the result.
+-/
+
+variables (p q r : Prop)
+
+/- Theorem : no-side-bar
+Let $p$, $q$, $r$ be propostions. Suppose $h : p \land (q \land r)$. Then $p \land q$ 
+follows.
+-/
+theorem and_summary (p r q : Prop) (h : p ∧ (q ∧ r)) : p ∧ q :=
+begin
+  cases h with hp hqr,
+  cases hqr with hq hr,
+  exact and.intro hp hq,
+
+
+
+
+
+
+
+
+end
+
+end exlean -- hide
