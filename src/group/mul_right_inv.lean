e8338e84a414a5b00d172728b13d9104d4163393
Gihan Marasingha <gihan.marasingha@gmail.com>
2022-02-20 14:51:35 +0000

    minor style changes (Lemma -> Theorem, etc.)

--- src/group/mul_right_inv.lean
+++ src/group/mul_right_inv.lean
@@ -22,7 +22,7 @@ In the previous level, we introduced the left inverse axiom for groups. In this 
 
 ### Tasks
 
-* By hand, write a proof that if $a$ is an element of a group $G$, then $a * a^{-1} = 1$. You may
+* By hand, write a proof that if $b$ is an element of a group $G$, then $b * b^{-1} = 1$. You may
 use any axioms or theorems proved in the previous level.
 
 * Complete the Lean 'proof by calculation' below.
@@ -31,7 +31,7 @@ use any axioms or theorems proved in the previous level.
 /- Theorem : 
 $b \ast b^{-1} = 1$.
 -/
-lemma mul_right_inv : b * b⁻¹ = 1 :=
+theorem mul_right_inv : b * b⁻¹ = 1 :=
 begin
 /- hint
   calc b * b⁻¹ = sorry : by sorry
